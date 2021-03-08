925495d6dc04f3cffc9bbbd76630bb7efce4f08b
Your Name <you@example.com>
2021-03-08 14:29:02 +0000

    Fix version

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+  * binary_tree_is_root - binary_tree_is_root check if it's a root
+  * @node: check the pointer
+  * Return: 0 false, 1 true
+  **/
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node->parent != NULL || node == NULL)
+		return (0);
+	else
+		return (1);
+}
