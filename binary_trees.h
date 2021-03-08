24cf14d95b43e0d8590c03fa592065a8d539e79b
TS-N <2501@holbertonschool.com>
2021-03-08 13:50:07 +0000

    fix header typo

--- binary_trees.h
+++ binary_trees.h
@@ -6,7 +6,6 @@
 #include <stdio.h>
 
 
-
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -55,6 +54,8 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+
 /* Advanced task's prototypes */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
@@ -72,8 +73,10 @@ avl_t *array_to_avl(int *array, size_t size);
 avl_t *avl_remove(avl_t *root, int value);
 avl_t *sorted_array_to_avl(int *array, size_t size);
 int binary_tree_is_heap(const binary_tree_t *tree);
-heap_t *heap_insert(heap_t **root, int value)
+heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+
+
 #endif
