db745441aa4e23a366f5d96cbcf076ea1b873fac
Your Name <you@example.com>
2021-03-09 23:04:47 +0000

    Find sibling

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - Finds the sibling 
+ * @node: the node
+ * Return: Sibling.
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *tmp;
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	tmp = node->parent;
+	if (tmp->left == node)
+		return (tmp->right);
+	return (tmp->left);
+}
