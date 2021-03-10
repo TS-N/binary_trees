c4bc8e92b342bda5eacfef7dfccec8c80d9157fb
Your Name <you@example.com>
2021-03-10 11:31:32 +0000

    fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,11 +14,11 @@ size_t tree_height(const binary_tree_t *tree)
 		if (tree == NULL)
 			return (0);
 		if (!tree->left)
-			return (1);
+			left = 1;
 		if (tree->left)
 			left = 1 + tree_height(tree->left);
 		if (!tree->right)
-			return (1);
+			right = 1;
 		if (tree->right)
 			right = 1 + tree_height(tree->right);
 		if (left != 0)
