48086d01a7cb62b67fc2df9c3e891c6eaca0ee42
Your Name <you@example.com>
2021-03-10 11:16:23 +0000

    fix

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,8 +8,10 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node->parent != NULL || node == NULL)
+	if (node == NULL)
 		return (0);
-	else
+	if (node->parent == NULL)
 		return (1);
+	else
+		return (0);
 }
